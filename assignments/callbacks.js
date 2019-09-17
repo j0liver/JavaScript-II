85afe681cfa6922f501450b7fb7cad4b0483e44c
Joe O <j0liverko@gmail.com>
2019-09-17 17:45:33 -0500

    le done

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -7,8 +7,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   // GIVEN THIS PROBLEM:
 
   function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
+    return cb(arr[0]);
 
   // SOLUTION:
 
@@ -40,24 +39,27 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr.getLength());
 }
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  return cb(arr[])
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x+y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item));
 }
 
 /* STRETCH PROBLEM */
